53a441689ef75fba416979eb1ed1b339ccb23d85
Takanori Suzuki <takanori@takanory.net>
2022-05-25 22:16:58 +0900

    ISSHA-2572 add settings for opengraph

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'PyCon JP Association'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxext.opengraph',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -43,7 +44,7 @@ language = 'ja'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['episode/template.rst']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,9 +52,22 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'pycontv_theme'
+html_theme_path = ["."]
+html_logo = '_static/logo.png'
+html_favicon = '_static/favicon.ico'
+html_css_files = ["css/custom.css"]
+html_title = 'PyCon JP TV'
+
+html_last_updated_fmt = '%Y-%m-%d'
+html_show_copyright = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# options for Open Graph
+# https://github.com/wpilibsuite/sphinxext-opengraph
+ogp_site_url = "https://tv.pycon.jp/"
+ogp_image = "https://tv.pycon.jp/_images/eyecatch.png"
